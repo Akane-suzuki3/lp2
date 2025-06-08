29b2bb4290f183ac2ce07576e04827480fe85cee
Akane-suzuki3 <akp.hq282@gmail.com>
2025-06-08 13:59:45 +0900

    0608_最新

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,35 +1,26 @@
-// ハンバーガーメニュー
-var hamburger = $('.hamburger-menu');
 
-// OPEN/CLOSEボタンをクリックしたら
-$('.hamburger-button').on('click', function () {
-    // console.log('click');
-    // .hamburgerの表示・非表示を繰り返す
-    hamburger.toggleClass('hamburger-menu-active');
 
+// カルーセル
+$('.cover-slick').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    fade: true
 });
 
-// 画面幅のサイズが変わったら
-$(window).on('resize', function () {
-    // console.log('resize');
-    // ハンバーガーメニューを閉じる
-    hamburger.removeClass('hamburger-menu-active');
-});
-
-// スマートタブの初期化
-$(document).ready(function () {
-    $('#smarttab').smartTab({
-        selected: 0,
-        theme: 'basic',
-        autoAdjustHeight: false,
-        backButtonSupport: false,
-    });
-
-    $('#smarttab').on("showTab", function (e, anchorObject, tabIndex) {
-        $('.tab-pane').removeClass('active');
-        $('.tab-pane').eq(tabIndex).addClass('active');
-    });
+$('#smarttab').smartTab({
+    enableUrlHash: false,
+    autoAdjustHeight: false
 });
 
 
 
+// アコーディオンのタイトルがクリックされたら
+$('.according-title-Q').on('click', function () {
+    // .accordion-contentを選択
+    var content = $(this).next();
+    // .accordion-contentを表示・非表示
+    content.slideToggle()
+    $(this).toggleClass('bi-caret-up-fill');
+    $(this).toggleClass('bi-caret-down-fill');
+});
